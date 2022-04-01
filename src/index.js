d888da0e3ee0d0ab0e91619d94bbbbfc4fb929b0
riversun <riversun.org@gmail.com>
2022-04-01 13:29:40 +0900

    update lib to 1.3.0

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ const opt = {
     api: {
         endpoint: 'http://localhost:8081/chat',//URL of chat server
         method: 'GET',                         //HTTP METHOD
-        dataType: 'json'                       //DATA TYPE
+        dataType: 'json',                       //DATA TYPE
+        escapeUserInput: true,// true:Escaping HTML tags in user input text when submitting to the server. default is false.
     },
     bot: {
         botPhoto: 'https://riversun.github.io/img/riversun_144.png',//URL of bot photo image
